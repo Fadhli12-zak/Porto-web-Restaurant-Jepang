78272a51055729f9b0ff8f604732d7c4dd25e85d
fadhlidzaki <fadhlidzaki120@gmail.com>
2024-09-10 20:37:51 +0700

    slider menu

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("halo");h
+const slides = document.querySelectorAll(".slide");
+let counter = 0;
+
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const prevSlide = () => {
+  counter--;
+  if (counter === -1) {
+    counter = 4;
+  }
+  slideImage();
+};
+const nextSlide = () => {
+  counter++;
+  if (counter === 5) {
+    counter = 0;
+  }
+  slideImage();
+};
+
+const slideImage = () => {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+    if (slide.style.transform > `translateX(-${400}%)`) {
+      slide.style.transform = `translateX(-${0}%)`;
+    }
+  });
+};
